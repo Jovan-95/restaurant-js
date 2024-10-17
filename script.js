d5f9965c018a5564d84875a74c5283fde770af8e
jovan <vuksanovicjovan@hotmail.com>
2024-10-17 14:23:39 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 /////!!!!! Restaurant Ordering System
+const menuWrapper = document.querySelector('.menu')
+const orderWrapper = document.querySelector('.order')
+const historyWrapper = document.querySelector('.history')
+const historyBtn = document.querySelector('.history-btn')
+
 const menuList = document.querySelector('#menu-items');
 const orderItems = document.querySelector('#order-items');
 const totalAmount = document.querySelector('#total-amount');
 const placeOrderBtn = document.querySelector('#place-order');
 const orderNumberDisplay = document.querySelector('#order-number')
+const createOrderBtn = document.querySelector('.create-order-btn');
+
 
 class MenuItem {
     constructor(name, price, category) {
@@ -30,12 +37,12 @@ class Order {
     }
 
     removeOrderItem(itemName) {
-        console.log(this.items = this.items.filter(item => item.name !== itemName))
+        // console.log(this.items = this.items.filter(item => item.name !== itemName))
         this.items = this.items.filter(item => item.name !== itemName)
     }
 
     calculateTotal() {
-        console.log(this.totalAmount = this.items.reduce((total, item) => total + item.price, 0));
+        // console.log(this.totalAmount = this.items.reduce((total, item) => total + item.price, 0));
         this.totalAmount = this.items.reduce((total, item) => total + item.price, 0);
     }
 
@@ -51,6 +58,7 @@ class Restaurant {
     constructor() {
         this.orders = [];
         this.menu = [];
+        this.searchedOrdersByNum = [];
     }
 
     addMenuItem(menuItem) {
@@ -72,6 +80,7 @@ class Restaurant {
     getOrderByNum(orderNum) {
         let orderByNum = this.orders.find(order => order.orderNumber === orderNum);
         console.log(orderByNum)
+        this.searchedOrdersByNum.push(orderByNum)
     }
 }
 
@@ -99,13 +108,26 @@ restaurant.addMenuItem(item6)
 // restaurant.removeMenuItem('Cake');
 // restaurant.getOrderByNum(1);
 
-let createdOrder = restaurant.createOrder();
 
 // createOrder.addOrderItem(item1)
 // createOrder.addOrderItem(item3)
 
 /// DOM
-console.log('-------------------------------------')
+let createdOrder;
+// createdOrder = restaurant.createOrder();
+
+createOrderBtn.addEventListener('click', function () {
+    console.log('Order is created')
+
+    createdOrder = restaurant.createOrder();
+    menuWrapper.classList.remove('d-none')
+    orderWrapper.classList.remove('d-none')
+    historyWrapper.classList.remove('d-none')
+
+    createOrderBtn.classList.add('d-none')
+
+
+})
 
 restaurant.menu.forEach((item) => {
     let createdOrderItem;
@@ -140,7 +162,6 @@ restaurant.menu.forEach((item) => {
 
         createdOrder.addOrderItem(item);
 
-        console.log(createdOrder)
         // console.log(restaurant)
 
         createdOrder.items.forEach((item) => {
@@ -152,6 +173,8 @@ restaurant.menu.forEach((item) => {
         orderItems.innerHTML += `<div style="color: blue; font-size: 24px;">ORDER NAME:${createdOrderItem.name}</div>`
 
         totalAmount.innerHTML = `<div>${createdOrder.totalAmount}$</div>`
+        // console.log(createdOrder)
+        console.log(restaurant)
 
     });
 
@@ -181,20 +204,59 @@ restaurant.menu.forEach((item) => {
 
 
     })
-
 });
 
 
 placeOrderBtn.addEventListener('click', function () {
     if (createdOrder.items.length > 0) {
-        orderNumberDisplay.innerHTML = `<div style="color: blue; font-size: 24px;">ORDER NUMBER:${createdOrder.orderNumber + 1}</div>`;
+        let li = menuList.querySelectorAll('li');
+        li.forEach(el => {
+            el.classList.remove('active');
+        });
+
+        orderNumberDisplay.innerHTML = `<div style="color: blue; font-size: 24px;">ORDER NUMBER:${createdOrder.orderNumber++}</div>`;
         orderItems.innerHTML = alert('Your order is created!');
         orderItems.innerHTML = `Your order is preparing...`
+        totalAmount.innerHTML = `<div>0</div>`;
 
+        // createdOrder.items = [];
+
+
+        createdOrder = restaurant.createOrder();
+        console.log(restaurant)
     } else {
         orderItems.innerHTML = alert('Your order is empty!!!');
         orderItems.innerHTML = `Add menu items to your order`
 
     }
-
 })
+
+historyBtn.addEventListener('click', function () {
+    restaurant.orders.forEach((order) => {
+        console.log(order);
+        historyWrapper.innerHTML += `<div class="history-nums">Order numbers: ${order.orderNumber}</div> `
+
+    })
+
+    if (restaurant.orders.length >= 1) {
+        let input = document.createElement('input');
+        input.type = 'number';
+        let searchOrderBtn = document.createElement('button')
+
+        historyWrapper.appendChild(input);
+        historyWrapper.appendChild(searchOrderBtn);
+        searchOrderBtn.innerHTML = 'Search order by number'
+
+        function searchOrderByNum() {
+            restaurant.getOrderByNum(Number(input.value));
+            console.log(restaurant.searchedOrdersByNum);
+            restaurant.searchedOrdersByNum.forEach((el) => {
+                historyWrapper.innerHTML += `<div>${el.orderNumber}, ${el.totalAmount}</div> `
+            })
+        }
+
+        searchOrderBtn.addEventListener('click', searchOrderByNum)
+    }
+})
+
+// Fix order history duplicate, list searched orders properly
